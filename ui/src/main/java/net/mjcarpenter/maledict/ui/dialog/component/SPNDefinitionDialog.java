85c80a9612b496fb62e487e73b1c6f7606b533a6
Mike Carpenter <rodents210@gmail.com>
2016-12-11 21:37:23 -0500

    Applied GNU LGPLv3

--- ui/src/main/java/net/mjcarpenter/maledict/ui/dialog/component/SPNDefinitionDialog.java
+++ ui/src/main/java/net/mjcarpenter/maledict/ui/dialog/component/SPNDefinitionDialog.java
@@ -1,3 +1,20 @@
+/*
+ * Maledict - An Interactive Tool for Learning Linear and Differential Cryptanalysis of SPNs
+ * Copyright (C) 2016  Mike Carpenter
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.mjcarpenter.maledict.ui.dialog.component;
 
 import java.awt.BorderLayout;
@@ -9,11 +26,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import javax.imageio.ImageIO;
 import javax.swing.JDialog;
@@ -47,7 +60,6 @@ import net.mjcarpenter.maledict.crypto.spn.SBox;
 import net.mjcarpenter.maledict.crypto.spn.SPNComponent;
 import net.mjcarpenter.maledict.crypto.spn.SPNetwork;
 import net.mjcarpenter.maledict.reports.CryptanalysisReport;
-import net.mjcarpenter.maledict.reports.SBoxReport;
 import net.mjcarpenter.maledict.ui.component.DummyFrame;
 import net.mjcarpenter.maledict.ui.component.KeyExtractionProgressDialog;
 import net.mjcarpenter.maledict.ui.dialog.ldc.ApproximationDialog;
@@ -58,7 +70,6 @@ import net.mjcarpenter.maledict.ui.model.RoundTreeNode;
 import net.mjcarpenter.maledict.ui.model.SPNTreeModel;
 import net.mjcarpenter.maledict.ui.model.SPNTreeNode;
 import net.mjcarpenter.maledict.ui.util.MasterPropertiesCache;
-import net.mjcarpenter.maledict.util.BitUtils;
 
 @SuppressWarnings("serial")
 public class SPNDefinitionDialog extends ComponentDefinitionDialog<SPNetwork> implements ActionListener
